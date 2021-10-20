bbc04501dc5f57ffd83fa64f3a3d94b356bbd5c6
Gülnisa Aslan <gulnisaslan@gmail.com>
2021-10-20 23:05:03 +0300

    18/10/2021-homework 20/10/2021

---
+++ javaScriptStarterKit/src/app.js
@@ -0,0 +1,18 @@
+console.log("Merhaba Kodlama.io");
+
+let dolarBugun=9.30;
+let dolarDun =9.20;
+
+
+console.log(dolarDun) 
+
+const euroDun=11.2;
+//euroDun=11; //bu kullanılmaz çünkü sabit değişkene farklı turden değişkem atayamayız.
+let konutKredileri=["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel konut kredisi"];
+console.log("<ul>")
+for (let i = 0; i < konutKredileri.length; i++) {
+   console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+
+console.log(konutKredileri);
